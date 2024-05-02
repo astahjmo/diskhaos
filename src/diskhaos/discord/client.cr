b2acc0da94e15068079c762f99e220befe7ec8f8
astahjmo <contato.johnatan373@gmail.com>
2024-05-02 09:01:00 -0300

    fix: websocket instance as a intance variable of a connected gateway

--- src/diskhaos/discord/client.cr
+++ src/diskhaos/discord/client.cr
@@ -3,7 +3,6 @@ require "log"
 require "random"
 require "http/client"
 
-
 DISCORD_URL = "discord.com"
 PORT = 443
 USER_AGENT = "diskhaos/1.0"
@@ -49,9 +48,13 @@ module Diskhaos
   end
 
   class Gateway
-      @@interval : Int32 = 0
-      @@indentified : Bool = false
-
+    @@interval : Int32 = 0
+    @@indentified : Bool = false
+    #Crystal check types at compile time and
+    #at this point of version 1.12 crystal cannot trust in control floow
+    #to assume when is or not Nil !
+    # TODO: moving @websocket to a new class and of websocket and make a new instance
+    # connection
     def initialize(token : String)
       Log.setup(:debug)
       @token = token
@@ -59,11 +62,30 @@ module Diskhaos
       @headers = HTTP::Headers.new
       @headers.add("Authorization", "Bot " + token)
       @headers.add("User-Agent", USER_AGENT)
+      @websocket = HTTP::WebSocket.new(host: "gateway.discord.gg", path: "/?v=10&encoding=json", port: 443, tls: true, headers: @headers)
+    end
+
+    private def start_websocket()
+      Log.debug{"Registering websocket handlers!"}
+      @websocket.on_message do |msg|
+        Log.debug{"Received a websocket message"}
+        message = handle_websocket_message(msg)
+        Log.debug{"Sending op code: #{message}"}
+        @websocket.send(message.to_s)
+      end
+      @websocket.on_close do |msg|
+        puts "CLOSED: #{msg}"
+      end
+      @websocket.on_ping do |msg|
+        puts "PINGED: #{msg}"
+      end
+      Log.debug{"Running websocket"}
+      @websocket.run
     end
 
     private def handle_websocket_message(msg)
         response = JSON.parse(msg).as_h
-        Log.debug{"Received a ws ping: #{Opcodes.to_string(response["op"])}"}
+        Log.debug{"Received a websocket ping: #{Opcodes.to_string(response["op"])}"}
         Log.debug{"#{response}"}
         if @@interval == 0
           @@interval = response["d"]["heartbeat_interval"].to_s.to_i
@@ -71,18 +93,6 @@ module Diskhaos
         real_interval = (Random.rand(0.0..1.0) * @@interval) / 1000.0
         sleep(real_interval)
         if @@interval != 0 && @@indentified == false
-          send = {
-            op: Opcodes::IDENTIFY,
-            d: {
-              token: @token,
-              intents: 513,
-              properties: {
-              os: "linux",
-              browser: "Diskhaos",
-              device: "Diskhaos"
-              }
-            }
-          }
         else
           send = {
             op: Opcodes::HEARTBEAT,
@@ -93,39 +103,26 @@ module Diskhaos
     end
 
     def connect
-      response = get_gateway_url()
-      Log.debug{"Getting discord websocket gateway"}
-      if response.success?
-        Log.debug{"Discord gatewaay #{response.body}"}
-        gateway_url = JSON.parse(response.body)["url"].to_s.sub("wss://", "")
-        start_websocket(gateway_url)
-      else
-        puts "Failed to get gateway URL: #{response.status_code} - #{response.body}"
-      end
+      start_websocket()
     end
 
-    private def get_gateway_url
-      @client.get("/api/v10/gateway/bot", @headers)
+    private def send_identify(msg)
+        data = {
+            token: @token,
+            intents: 513,
+            properties: {
+            os: "linux",
+            browser: "Diskhaos",
+            device: "Diskhaos"
+            }
+        }
+        send_package(Opcodes::DISPATCH)
     end
-
-    # TODO: Stop using HTTP::WebSocket and implement new way to deal with this
-    private def start_websocket(gateway_url)
-      Log.debug{"Registering ws handlers!"}
-      ws = HTTP::WebSocket.new(host: gateway_url, path: "/?v=10&encoding=json", port: 443, tls: true, headers: @headers)
-      ws.on_message do |msg|
-        Log.debug{"Received a websocket message"}
-        message = handle_websocket_message(msg)
-        Log.debug{"Sending op code: #{message}"}
-        ws.send(message.to_s)
-      end
-      ws.on_close do |msg|
-        puts "CLOSED: #{msg}"
-      end
-      ws.on_ping do |msg|
-        puts "PINGED: #{msg}"
-      end
-      Log.debug{"Running WS"}
-      ws.run
+    private def send_package(opcode, data)
+        send = {
+          op: Opcodes::IDENTIFY,
+          d: data
+          }
     end
   end
 end
